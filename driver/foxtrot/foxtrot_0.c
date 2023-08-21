4124a766cae2cc41e0944e6d079ebd974fe8208d
Timothy Fraser <tim.fraser@tjfc-llc.com>
2023-08-21 11:16:24 -0400

    Fixing bug 2023-07-05-read-timeout in all Foxtrot drivers.

--- driver/foxtrot/foxtrot_0.c
+++ driver/foxtrot/foxtrot_0.c
@@ -1,16 +1,17 @@
-// Copyright (c) 2022 Provatek, LLC.
+/* Copyright (c) 2022 Provatek, LLC.
+ * Copyright (c) 2023 Timothy Jon Fraser Consulting LLC.
+ */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
-#include <time.h>
 
+#include "clock.h"
 #include "framework.h"
-#include "driver.h"
 #include "device_emu.h"
+#include "driver.h"
 
-#define NAND_POLL_INTERVAL_US 10  /* polling interval in microseconds */
 
 volatile unsigned long* driver_ioregister;
 
@@ -23,27 +24,21 @@ void nand_set_register(unsigned int offset, unsigned char value)
 // Waits for device status to be ready for an action
 int nand_wait(unsigned int interval_us)
 {
-	/* Some explanation on this timeout computation:
-	 *
-	 * We're trying to mimic what real Linux device drivers see: a
+	/* We're trying to mimic what real Linux device drivers see: a
 	 * volatile jiffies variable whose value increases
-	 * monotonically with clock ticks.  The clock() function has
-	 * similar behavior.  My Linux's bits/time.h indicates that
-	 * clock_t ticks are always microseconds, so rather than
-	 * converting microseconds to ticks using CLOCKS_PER_SEC /
-	 * 1000000, I'm just adding.  Although I worry that I'm losing
-	 * POSIX points by doing so, the simple addition mimics the
-	 * pattern real Linux device drivers would use.
+	 * monotonically with clock ticks.  The now() function has
+	 * similar behavior.
 	 */
-	clock_t timeout = clock() + interval_us;
-	
-	while(clock() < timeout) {
+	timeus_t timeout = now() + interval_us;
+
+	do {
 		if (gpio_get(PN_STATUS) == DEVICE_READY) {
 			return 0;
 		}
 		usleep(NAND_POLL_INTERVAL_US);
-	}
-	return -1;
+	} while(now() < timeout);
+
+	return ((gpio_get(PN_STATUS) == DEVICE_READY) ? 0 : -1);
 }
 
 // Reads the data in to buffer in the nand device at offset with length of size
